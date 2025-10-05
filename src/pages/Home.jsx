a0c525d34793ab2d6ea6792803ccb0e9eda5ec86
AARUL SHARMA <saarul267@gmail.com>
2025-10-05 23:30:17 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import Footer from "../components/Footer/Footer";
 import Header from "../components/Header/Header";
-import Layout  from "../components/Layout/Layout";
+import Layout  from "../components/Layout/Layout.jsx";
 
 function Home() {
   return (
